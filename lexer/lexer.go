55b49e247d69ee490f9fea9eff686858457674dc
theBashShell <edohbruno@gmail.com>
2021-03-16 20:50:55 +0000

    lexer + test

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,12 @@
 package lexer
 
+import (
+	"log"
+	"regexp"
+
+	"github.com/thebashshell/simply/token"
+)
+
 type Lexer struct {
 	ch               byte
 	input            string
@@ -22,4 +29,98 @@ func (l *Lexer) readChar() {
 
 	l.position = l.offset
 	l.offset++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.eatWhitespace()
+
+	switch l.ch {
+	case 0:
+		tok = token.Token{Kind: token.EOF}
+	case '=':
+		tok = makeToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = makeToken(token.PLUS, l.ch)
+	case ',':
+		tok = makeToken(token.COMMA, l.ch)
+	case ';':
+		tok = makeToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = makeToken(token.LPAREN, l.ch)
+	case ')':
+		tok = makeToken(token.RPAREN, l.ch)
+	case '{':
+		tok = makeToken(token.LBRACE, l.ch)
+	case '}':
+		tok = makeToken(token.RBRACE, l.ch)
+
+	default:
+		if isDigit(l.ch) {
+			tok.Value = l.readNumber()
+			tok.Kind = token.INT
+			return tok
+		} else if isLetter(l.ch) {
+			tok.Value = l.getIdentifier()
+			tok.Kind = token.LookUpIdentifier(tok.Value)
+		} else {
+			tok = makeToken(token.ILLEGAL, l.ch)
+			return tok
+		}
+
+	}
+
+	l.readChar()
+	return tok
+}
+
+func makeToken(tok string, ch byte) token.Token {
+	return token.Token{Kind: tok, Value: string(ch)}
+}
+
+func (l *Lexer) getIdentifier() string {
+	pos := l.position
+
+	if isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[pos:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	pos := l.position
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[pos:l.position]
+}
+
+func isLetter(ch byte) bool {
+	ok, err := regexp.MatchString(`[a-zA-Z_]`, string(ch))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ok
+}
+
+func isDigit(ch byte) bool {
+	ok, err := regexp.MatchString(`[0-9]`, string(ch))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ok
+}
+
+func (l *Lexer) eatWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
---
+++ lexer/lexer_test.go
@@ -0,0 +1,38 @@
+package lexer
+
+import (
+	"github.com/thebashshell/simply/token"
+
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+	tests := []struct {
+		expectedType    string
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Kind != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Kind)
+		}
+		if tok.Value != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Value)
+		}
+	}
+}
