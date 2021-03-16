d9f788b0c4d05677ad9ef8b6b4aed3659cc872c7
theBashShell <edohbruno@gmail.com>
2021-03-16 22:54:35 +0000

    AST added

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+package ast
+
+import (
+	"github.com/thebashshell/simply/token"
+)
+
+type Node interface {
+	TokenValue() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenValue() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenValue()
+	}
+
+	return ""
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) TokenValue() string {
+	return i.Token.Value
+}
+
+func (i *Identifier) expressionNode() {}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (l *LetStatement) statementNode() {}
+func (l *LetStatement) TokenValue() string {
+	return l.Token.Value
+}
