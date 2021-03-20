4bd614a2b5455034be1a239639646ddbc31438c7
theBashShell <edohbruno@gmail.com>
2021-03-20 20:23:53 +0000

    ast updated

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,14 @@
 package ast
 
 import (
+	"bytes"
+
 	"github.com/thebashshell/simply/token"
 )
 
 type Node interface {
 	TokenValue() string
+	String() string
 }
 
 type Statement interface {
@@ -22,6 +25,14 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 func (p *Program) TokenValue() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenValue()
@@ -35,6 +46,8 @@ type Identifier struct {
 	Value string
 }
 
+func (i *Identifier) String() string { return i.Value }
+
 func (i *Identifier) TokenValue() string {
 	return i.Token.Value
 }
@@ -47,6 +60,19 @@ type LetStatement struct {
 	Value Expression
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenValue() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
 func (l *LetStatement) statementNode() {}
 func (l *LetStatement) TokenValue() string {
 	return l.Token.Value
@@ -57,6 +83,16 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenValue() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
 func (rs *ReturnStatement) statementNode()     {}
 func (rs *ReturnStatement) TokenValue() string { return rs.Token.Value }
 
@@ -65,5 +101,12 @@ type ExpressionStatement struct {
 	Expression Expression
 }
 
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
 func (es *ExpressionStatement) statementNode()       {}
-func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Value }
