d1b8fe9eb853002ecedb59b109126d42854cb0be
theBashShell <edohbruno@gmail.com>
2021-03-20 20:49:57 +0000

    prefix expression node added

--- ast/ast.go
+++ ast/ast.go
@@ -119,3 +119,20 @@ type IntegerLiteral struct {
 func (il *IntegerLiteral) expressionNode()    {}
 func (il *IntegerLiteral) TokenValue() string { return il.Token.Value }
 func (il *IntegerLiteral) String() string     { return il.Token.Value }
+
+type PrefixExpression struct {
+	Token    token.Token // The prefix token, e.g. !
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()    {}
+func (pe *PrefixExpression) TokenValue() string { return pe.Token.Value }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
